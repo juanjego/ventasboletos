747c9a770c41e30da3cb063cc1bacf1334a87874
Abdi0804 <66394551+Abdi0804@users.noreply.github.com>
2020-08-01 02:12:44 -0500

    Abdi

---
+++ src/app/components/catalogo-c/catalogo-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-c',
+  templateUrl: './catalogo-c.component.html',
+  styleUrls: ['./catalogo-c.component.css']
+})
+export class CatalogoCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
